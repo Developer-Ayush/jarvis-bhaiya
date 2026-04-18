fcce8f4c6c7048ad22415c75e6e96460107e1928
Ayush Anand <developerayushanand@gmail.com>
2026-04-18 18:05:20 +0530

    Remove voice tag from SSML response

--- api/index.py
+++ api/index.py
@@ -22,7 +22,7 @@ COHERE_KEY     = os.environ.get("CohereApiKey", "")
 
 def matthew(text: str) -> str:
     text = text.replace("&", "and").replace("<", "").replace(">", "")
-    return f'<speak><voice name="Kajal">{text}</voice></speak>'
+    return f'<speak>{text}</speak>'
 
 def _build_response(text, end_session, reprompt=None, directives=None):
     response = {
